c613e4f6a1ca546d3532b04cfd7a3779926cef98
Andy Eaton <aeaton@thevibrand.com>
2014-02-13 17:04:53 -0600

    Part 1

--- IGotThisShit.Web/Models/ExternalLogin.cs
+++
@@ -1,9 +0,0 @@
-namespace IGotThisShit.Web.Models
-{
-    public class ExternalLogin
-    {
-        public string Provider { get; set; }
-        public string ProviderDisplayName { get; set; }
-        public string ProviderUserId { get; set; }
-    }
-}
